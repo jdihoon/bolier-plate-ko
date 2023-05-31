56a377446563d71bd274bed41a2a7786b8f3bf4f
jdihoon <jdihoon@douzone.com>
2023-05-31 20:31:56 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -3,16 +3,45 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const { User } = require('./models/User');
+// const bodyParser = require('body-parser');
+const config = require('./config/key');
+
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+// application/json
+app.use(express.json());
 
 const mongoose = require('mongoose');
-const url = 'mongodb+srv://jdihoon:mdmc7508@cluster0.9u28y7a.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(url)
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connnected...'))
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', async (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  await user
+          .save()
+          .then(() => {
+            res.status(200).json({
+              success: true,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.json({
+              success: false,
+              err: err,
+            });
+          });
+  
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
